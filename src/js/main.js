d7e8dfff15442ad24c6487d92e390cf34b7b9e42
Riku Rouvila <riku.rouvila@gmail.com>
2015-01-16 19:22:33 +0200

    first layout ideas

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log('foobar');
+'use strict';
+
+var request = require('axios');
+var form = document.getElementById('invite');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.classList.remove('has-success', 'has-error');
+  var email = event.target.elements.email.value;
+
+  request.post('/api/invites', {
+    email: email
+  }).then(function() {
+    form.classList.add('has-success');
+  }).catch(function() {
+    form.classList.add('has-error');
+  });
+});
