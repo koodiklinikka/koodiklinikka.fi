954a08c6246661d77a582a6e2f1e0e78325b675f
Riku Rouvila <riku.rouvila@gmail.com>
2015-03-22 16:01:24 +0200

    change remote api url from http to https

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,7 @@ gulp.task('server', function() {
 
   var proxy = httpProxy.createProxyServer({
     changeOrigin: true,
-    target: process.env.SERVER || 'http://koodiklinikka.fi/api'
+    target: process.env.SERVER || 'https://koodiklinikka.fi/api'
   });
 
   proxy.on('error', function(err) {
