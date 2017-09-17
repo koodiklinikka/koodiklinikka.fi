eaf50b658cead7c0da8e373d7636ca53b69c28cd
Riku Rouvila <riku.rouvila@gmail.com>
2017-09-17 15:09:20 +0100

    remove debug echos

--- deploy-surge.sh
+++ deploy-surge.sh
@@ -40,11 +40,9 @@ do
   DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
   DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
-  echo "test1"
   echo "$TRAVIS_PULL_REQUEST"
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
-    echo "test"
     # Using the Issues api instead of the PR api
     # Done so because every PR is an issue, and the issues api allows to post general comments,
     # while the PR api requires that comments are made to specific files and specific commits
