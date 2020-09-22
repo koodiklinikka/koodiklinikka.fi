5e9b493a49db0cd2d4b1275a1f8c6ccde345c8e0
Aarni Koskela <akx@iki.fi>
2020-09-22 18:30:54 +0300

    DRY meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,50 +1,33 @@
 import React from "react";
 import Head from "next/head";
 
+const metaImage = "/static/images/meta.jpg";
+const metaDescription =
+  "Koodiklinikka on Suomen suurin ohjelmistoalan yhteisö, joka kokoaa työntekijät, harrastajat ja vasta-alkajat yhteen. Tarkoituksenamme on yhdistää ja kasvattaa suomalaista ohjelmointiyhteisöä, sekä tarjota apua ja uusia kontakteja ohjelmoinnista innostuneille nuorille.";
+const metaShortDescription =
+  "Koodiklinikka on suomalainen yhteisö ohjelmistoalan harrastajille ja ammattilaisille.";
+const metaTitle = "Koodiklinikka – Suomen suurin ohjelmistoalan yhteisö";
+const metaKeywords =
+  "ohjelmointi,frontend,open source,devaus,suomi,javascript,clojure,go,java,node.js,io.js,angular.js,web";
+const metaUrl = "https://koodiklinikka.fi/";
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta
-          name="title"
-          content="Koodiklinikka – Suomen suurin ohjelmistoalan yhteisö"
-        />
-        <meta
-          name="description"
-          content="Koodiklinikka on suomalainen yhteisö ohjelmistoalan harrastajille ja ammattilaisille."
-        />
-        <meta
-          name="keywords"
-          content="ohjelmointi,frontend,open source,devaus,suomi,javascript,clojure,go,java,node.js,io.js,angular.js,web"
-        />
+        <meta name="title" content={metaTitle} />
+        <meta name="description" content={metaShortDescription} />
+        <meta name="keywords" content={metaKeywords} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://koodiklinikka.fi/" />
-        <meta
-          property="og:title"
-          content="Koodiklinikka – Suomen suurin ohjelmistoalan yhteisö"
-        />
-        <meta
-          property="og:description"
-          content="Koodiklinikka on Suomen suurin ohjelmistoalan yhteisö, joka kokoaa työntekijät, harrastajat ja vasta-alkajat yhteen. Tarkoituksenamme on yhdistää ja kasvattaa suomalaista ohjelmointiyhteisöä, sekä tarjota apua ja uusia kontakteja ohjelmoinnista innostuneille nuorille."
-        />
-        <meta
-          property="og:image"
-          content="https://koodiklinikka.fi/static/images/meta.jpg"
-        />
+        <meta property="og:url" content={metaUrl} />
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:image" content={metaImage} />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://koodiklinikka.fi/" />
-        <meta
-          property="twitter:title"
-          content="Koodiklinikka – Suomen suurin ohjelmistoalan yhteisö"
-        />
-        <meta
-          property="twitter:description"
-          content="Koodiklinikka on Suomen suurin ohjelmistoalan yhteisö, joka kokoaa työntekijät, harrastajat ja vasta-alkajat yhteen. Tarkoituksenamme on yhdistää ja kasvattaa suomalaista ohjelmointiyhteisöä, sekä tarjota apua ja uusia kontakteja ohjelmoinnista innostuneille nuorille."
-        />
-        <meta
-          property="twitter:image"
-          content="https://koodiklinikka.fi/static/images/meta.jpg"
-        />
+        <meta property="twitter:url" content={metaUrl} />
+        <meta property="twitter:title" content={metaTitle} />
+        <meta property="twitter:description" content={metaDescription} />
+        <meta property="twitter:image" content={metaImage} />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -66,7 +49,6 @@ function MyApp({ Component, pageProps }) {
           href="/static/icons/favicon-16x16.png"
         />
         <link rel="manifest" href="/static/icons/site.webmanifest" />
-        <meta property="og:image" content="images/logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
