a00450dfb5407bcd241748a0c26cd03b06caff23
Riku Rouvila <riku.rouvila@gmail.com>
2022-09-25 10:56:25 +0300

    migrate koodiklinikka backend to fly.io

--- components/api.ts
+++ components/api.ts
@@ -1,4 +1,4 @@
-const host = process.env.SERVER || "https://koodiklinikka-api.herokuapp.com/";
+const host = process.env.SERVER || "https://koodiklinikka-api.fly.dev/";
 
 export default function getApiURL(path) {
   return host + path;
